435e7c40fe00981440f4d0107546cf476a7ea283
jacikot <tj180023d@student.etf.bg.ac.rs>
2021-05-30 14:58:05 +0200

    changed design of que.php

--- Faza V/public/assets/js/que.js
+++ Faza V/public/assets/js/que.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
 
     function validateQuestions(){
         let inputs =document.getElementsByTagName("input");
+        let add=0;
+        if(inputs.length==3)add=1;
         for (let i=0;i<inputs.length;i++){
-            document.getElementById("err"+i).style.display="none";
+            document.getElementById("err"+(i+add)).style.display="none";
         }
         let correct=true;
         let questions=[];
         for (let i=0;i<inputs.length;i++){
             if(inputs[i].value==""){
-                document.getElementById("err"+i).style.display="block";
+                document.getElementById("err"+(i+add)).style.display="block";
                 correct=false;
             }
             else {
@@ -31,14 +33,15 @@ function addUsername(){
     let newCol1=document.createElement("td");
     let question=document.createElement("p");
     let newCol2=document.createElement("td");
+    newCol2.classList.add("align-middle");
     question.classList.add("text");
     question.id="p3";
     question.innerText="Username:"
     newCol1.appendChild(question);
     newRow.appendChild(newCol1);
-    newCol2.innerHTML=" <input type=\"text\" id=\"t1\" class=\"username\"/>\n" +
+    newCol2.innerHTML=" <input type=\"text\" id=\"t1\" class=\"username border-bottom\"/>\n" +
         "                    <br>\n" +
-        "                    <div id=\"err3\" class=\"err\" >Polje nije uneseno!</div>"
+        "                    <div id=\"err0\" class=\"err\" >Polje nije uneseno!</div>"
     newRow.appendChild(newCol2);
     table.insertBefore(newRow,table.firstChild);
 }
@@ -50,7 +53,8 @@ function guestMode(){
     let button=document.getElementById("backbutton");
     button.innerText="Back";
     addUsername();
-    let env=document.getElementsByClassName("env");
-    env[0].style.height="68%";
+    document.getElementsByClassName("row")[0].style.marginTop="0px";
+    // let env=document.getElementsByClassName("env");
+    // env[0].style.height="68%";
 }
 
