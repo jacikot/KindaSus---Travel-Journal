4bc6310d8a5f024c21ad3e92129ce2e11eb8b16b
jacikot <jana.toljaga725@yahoo.com>
2021-10-01 17:38:32 +0200

    fixed

--- Faza V/public/assets/js/map.js
+++ Faza V/public/assets/js/map.js
@@ -2,7 +2,7 @@ google.charts.load('current', {
     'packages':['geochart'],
     // Note: you will need to get a mapsApiKey for your project.
     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-    'mapsApiKey': 'AIzaSyAMyavLfyTeuP6EDGSE_oLq8S5RkPjooMw'
+    'mapsApiKey': 'enter API KEY'
 });
   google.charts.setOnLoadCallback(drawRegionsMap);
   function drawMap(countries){
